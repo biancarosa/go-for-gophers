e300d08968df812e63bda4ea9bb206afd136d59c
Bianca Rosa <me@biancarosa.com.br>
2017-11-19 10:27:43 -0200

    add scheduler

---
+++ scheduler/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func cpuIntensive(p *int) {
+	for i := 0; i <= 100000000; i++ {
+		*p = i
+	}
+}
+
+func main() {
+	runtime.GOMAXPROCS(1)
+
+	x := 0
+	go cpuIntensive(&x)
+
+	time.Sleep(1 * time.Millisecond)
+
+	fmt.Println("terminou", x)
+}
